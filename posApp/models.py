7dee1cbbcd19f5b070b785c9bb7967dfdcc894aa
SuperPeterl <87328821+SuperPeterl@users.noreply.github.com>
2023-03-13 13:52:23 +0700

    up

--- posApp/models.py
+++ posApp/models.py
@@ -5,26 +5,26 @@ from django.utils import timezone
 
 # Create your models here.
 
-# class Employees(models.Model):
-#     code = models.CharField(max_length=100,blank=True) 
-#     firstname = models.TextField() 
-#     middlename = models.TextField(blank=True,null= True) 
-#     lastname = models.TextField() 
-#     gender = models.TextField(blank=True,null= True) 
-#     dob = models.DateField(blank=True,null= True) 
-#     contact = models.TextField() 
-#     address = models.TextField() 
-#     email = models.TextField() 
-#     department_id = models.ForeignKey(Department, on_delete=models.CASCADE) 
-#     position_id = models.ForeignKey(Position, on_delete=models.CASCADE) 
-#     date_hired = models.DateField() 
-#     salary = models.FloatField(default=0) 
-#     status = models.IntegerField() 
-#     date_added = models.DateTimeField(default=timezone.now) 
-#     date_updated = models.DateTimeField(auto_now=True) 
+class Employees(models.Model):
+    code = models.CharField(max_length=100,blank=True) 
+    firstname = models.TextField() 
+    middlename = models.TextField(blank=True,null= True) 
+    lastname = models.TextField() 
+    gender = models.TextField(blank=True,null= True) 
+    dob = models.DateField(blank=True,null= True) 
+    contact = models.TextField() 
+    address = models.TextField() 
+    email = models.TextField() 
+#department_id = models.ForeignKey(Department, on_delete=models.CASCADE) 
+    position_id = models.ForeignKey(Position, on_delete=models.CASCADE) 
+    date_hired = models.DateField() 
+    salary = models.FloatField(default=0) 
+    status = models.IntegerField() 
+    date_added = models.DateTimeField(default=timezone.now) 
+    date_updated = models.DateTimeField(auto_now=True) 
 
-    # def __str__(self):
-    #     return self.firstname + ' ' +self.middlename + ' '+self.lastname + ' '
+    def __str__(self):
+        return self.firstname + ' ' +self.middlename + ' '+self.lastname + ' '
 class Category(models.Model):
     name = models.TextField()
     description = models.TextField()
